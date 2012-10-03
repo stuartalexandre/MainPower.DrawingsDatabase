ef6481bad61ae211957c7344dc683cabf1caf77f
HamishC <hamish.cumming@gmail.com>
2012-10-03 20:31:55 +1300

    forgot to add some attached behaviours

---
+++ MainPower.DrawingsDatabase.GUI/BooleanToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MainPower.DrawingsDatabase.Gui
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool param = bool.Parse(value.ToString());
+            if (param == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
